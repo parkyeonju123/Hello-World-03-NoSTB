2b9c5dccd64187dc00572ba61a0e1f093e008a8e
D7702_09 <d7702_09@d7702-09ui-imac.local>
2018-04-16 16:50:51 +0900

    add HelloWorld

--- Hello World 03 NoSTB/ViewController.swift
+++ Hello World 03 NoSTB/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let mylbl = UILabel(frame: CGRect(x: 100, y:100, width:150, height:30))
+        mylbl.text = "Hello World"
+        mylbl.backgroundColor = UIColor.yellow
+        mylbl.textAlignment = NSTextAlignment.center
+        
+        view.addSubview(mylbl)
     }
 
     override func didReceiveMemoryWarning() {
